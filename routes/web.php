1c388d679c35460d82fc2c9b0a6dc02cee782b4e
Muhammad Hannan <mdhannan.info@gmail.com>
2025-09-27 17:45:43 +0600

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 use Sparktro\Installer\Http\Controllers\SecurityController;
 
+Route::prefix('install')->group(function () {
+    Route::get('/', [SecurityController::class, 'requirements'])->name('install.requirements');
+    Route::post('/database', [SecurityController::class, 'database'])->name('install.database');
+    Route::get('/admin', [SecurityController::class, 'adminForm'])->name('install.admin.form');
+    Route::post('/admin', [SecurityController::class, 'adminStore'])->name('install.admin.store');
+    Route::get('/finish', [SecurityController::class, 'finish'])->name('install.finish');
+});
+
 
 Route::prefix('install')
     ->name('install.')
