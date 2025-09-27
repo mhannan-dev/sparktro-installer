4b34ea8b5ef0bb4defbd34b02bec58578f0580a7
Muhammad Hannan <mdhannan.info@gmail.com>
2025-09-27 16:42:12 +0600

    Update

--- src/SparkTroInstallerServiceProvider.php
+++ src/SparkTroInstallerServiceProvider.php
@@ -8,10 +8,8 @@ class SparkTroInstallerServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Load package routes
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
-        // Load package views with proper namespace
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'installer');
     }
 
