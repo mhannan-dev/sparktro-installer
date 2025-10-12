6204e06e28b54b108a77c118449c9da1771c8316
Muhammad Hannan <mdhannan.info@gmail.com>
2025-10-12 22:18:04 +0600

    Update

--- src/Middleware/CheckInstallation.php
+++ src/Middleware/CheckInstallation.php
@@ -7,16 +7,33 @@ use Illuminate\Http\Request;
 
 class CheckInstallation
 {
+    // public function handle(Request $request, Closure $next)
+    // {
+    //     $appSecurity = filter_var(env('APP_SECURITY', false), FILTER_VALIDATE_BOOLEAN);
+
+    //     if ($appSecurity === true && $request->is('install*')) {
+    //         return redirect('login');
+    //     }
+
+    //     if ($appSecurity !== true && $request->is('install*')) {
+    //         return $next($request);
+    //     }
+
+    //     return $next($request);
+    // }
+
     public function handle(Request $request, Closure $next)
     {
         $appSecurity = filter_var(env('APP_SECURITY', false), FILTER_VALIDATE_BOOLEAN);
 
-        if ($appSecurity === true && $request->is('install*')) {
-            return redirect('login');
+        if (!$appSecurity) {
+            if (!$request->is('install*')) {
+                return redirect()->route('install.requirements');
+            }
         }
 
-        if ($appSecurity !== true && $request->is('install*')) {
-            return $next($request);
+        if ($appSecurity && $request->is('install*')) {
+            return redirect('/');
         }
 
         return $next($request);
